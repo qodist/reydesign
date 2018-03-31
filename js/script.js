f77ead4077565488e9545c060d54509997cba2d9
qodist <reyno75@gmail.com>
2018-03-31 03:28:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+var imgs = document.querySelectorAll(".imgFrame.press");
+
+for(var i = 0; i < imgs.length; i++){
+    imgs[i].addEventListener("click", function(){
+        this.classList.remove("press");
+    });
+}
+*/
