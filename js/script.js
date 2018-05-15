0e80029cba9f04edd5b33e25af457420f2246903
qodist <reyno75@gmail.com>
2018-05-15 20:55:03 +0300

    adding toggle nav

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-/*
-var imgs = document.querySelectorAll(".imgFrame.press");
-
-for(var i = 0; i < imgs.length; i++){
-    imgs[i].addEventListener("click", function(){
-        this.classList.remove("press");
-    });
+function myFunction(x) {
+    x.classList.toggle("change");
 }
-*/
