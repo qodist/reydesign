312cc91eb31c453b205d244a3e703ac12d9dc2bd
qodist <reyno75@gmail.com>
2018-05-16 19:30:49 +0300

    added flexbox and grid to straighten up the feature cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+//CLOSE MOBILE MENU
+var nav = document.querySelector(".mobile-nav");
+var checkbox = document.querySelector(".mobile-nav__checkbox");
+var items = document.querySelectorAll(".mobile-nav__item");
+
+items.forEach(function(item){
+    item.addEventListener("click", function(){
+        checkbox.checked = false;
+    });
+});
